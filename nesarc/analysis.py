76e11dc1d33268a150cea717217d77148050880c
Claudio Pose <claudio@pose.ch>
2017-04-10 22:22:24 +0200

    Added option to limit the numbers of rows and columns to be imported

--- nesarc/analysis.py
+++ nesarc/analysis.py
@@ -11,13 +11,22 @@ import library as lib
 
 if __name__ == "__main__":
     dfh_overall = lib.DataFrameHelper('C:\\Users\\claud\\OneDrive\\Dokumente\\Weiterbildung\\Data Analysis\\Coursera\\Wesleyan University\\Data Analysis and Interpretation Specialization\\Datasets\\NESARC\\nesarc_pds.csv',
-                                      variable_definition_path='C:\\Users\\claud\\OneDrive\\Dokumente\\Weiterbildung\\Data Analysis\\Coursera\\Wesleyan University\\Data Analysis and Interpretation Specialization\\Source Code\\nesarc\\predefined_variables.json')
+                                      variable_definition_path='C:\\Users\\claud\\OneDrive\\Dokumente\\Weiterbildung\\Data Analysis\\Coursera\\Wesleyan University\\Data Analysis and Interpretation Specialization\\Source Code\\nesarc\\predefined_variables.json',
+                                      limit_columns=True,
+                                      nrows=100)
 
     dfh_overall.import_data()
 
-    # Treat answers labelled with "Unknown" as Missing Data (NaN)
+    # Treat answers that fell into the category "Unknown" as missing data (NaN)
+    dfh_overall.df.S12Q1  = dfh_overall.df.S12Q1.replace('Unknown', numpy.nan)
+    dfh_overall.df.S12Q3D  = dfh_overall.df.S12Q3D.replace('Unknown', numpy.nan)
+    dfh_overall.df.S12Q3I  = dfh_overall.df.S12Q3I.replace('Unknown', numpy.nan)
     dfh_overall.df.S12Q2A12 = dfh_overall.df.S12Q2A12.replace('Unknown', numpy.nan)
-    # TODO: Recode other variables that will be used for testing the hypothesis
+    dfh_overall.df.S11AQ1B13 = dfh_overall.df.S11AQ1B13.replace('Unknown', numpy.nan)
+    dfh_overall.df.S11AQ1B20 = dfh_overall.df.S11AQ1B20.replace('Unknown', numpy.nan)
+    dfh_overall.df.S11AQ1B23 = dfh_overall.df.S11AQ1B23.replace('Unknown', numpy.nan)
+    dfh_overall.df.S11AQ1B24 = dfh_overall.df.S11AQ1B24.replace('Unknown', numpy.nan)
+    dfh_overall.df.S11AQ1B21 = dfh_overall.df.S11AQ1B21.replace('Unknown', numpy.nan)
 
     # Print overall distribution of the variable S12Q2A12 (illegally raised money for gambling)
     frequencey_analysis = lib.FrequencyAnalysis(df_helper=dfh_overall)
@@ -27,9 +36,9 @@ if __name__ == "__main__":
     dfh_sub_illegal_childhood = copy.copy(dfh_overall)
     dfh_sub_illegal_childhood.df = dfh_overall.df.copy()
 
-    dfh_sub_illegal_childhood.df = dfh_sub_illegal_childhood.df[(dfh_sub_illegal_childhood.df.S11AQ1B23 == 'Yes') |
-                                                                (dfh_sub_illegal_childhood.df.S11AQ1B24 == 'Yes') |
-                                                                (dfh_sub_illegal_childhood.df.S11AQ1B21 == 'Yes')]
+    dfh_sub_illegal_childhood.df = dfh_sub_illegal_childhood.df[(dfh_sub_illegal_childhood.df.S11AQ1B24 == 'Yes') &
+                                                                (dfh_sub_illegal_childhood.df.S11AQ1A21 == 'Yes')]
+
 
     # Print distribution of the variable S12Q2A12 for the subset created above
     frequencey_analysis.df_helper = dfh_sub_illegal_childhood
